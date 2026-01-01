6d8138180b24faf547e9c6a2e38ae893a707e609
juanserealpe <juansevarona@hotmail.com>
2026-01-01 11:54:27 -0500

    chore: initialize hexagonal architecture and DDD structure
    Set up the base project structure using Hexagonal Architecture
    and DDD, separating domain, application, infrastructure,
    and interface layers.

---
+++ src/main/java/co/unicauca/edu/unisched/UniSchedApplication.java
@@ -0,0 +1,13 @@
+package co.unicauca.edu.unisched;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniSchedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniSchedApplication.class, args);
+    }
+
+}
